a673472efe5afae5fba5059d42f39e03a1916e12
Smit <smitbhansali7@gmail.com>
2023-01-31 17:42:12 +0530

    create user step1 page done

--- angular/quantum-angular/src/app/app.module.ts
+++ angular/quantum-angular/src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { NavbarComponent } from './components/signup/navbar/navbar.component';
+import { Step1Component } from './components/signup/step1/step1.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    SignupComponent,
+    NavbarComponent,
+    Step1Component
   ],
   imports: [
     BrowserModule,
